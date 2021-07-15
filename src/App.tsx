d31b461238dc55831d63d74f56259460196e46d2
jayden <jayden@kcd.co.kr>
2021-07-16 03:28:56 +0900

    불필요한 코드삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-// TODO Change method signature [Easy]
-// 여기는 포함되나?
 function App() {
   return <div className="App">hello world</div>;
-}
-
-/**
- * TODO action text
- * line
- * line
- */
-function actionText() {
-  console.log("action text");
-}
-
-// TODO action text 2
-// here?
-function actionText2() {
-  console.log("action text");
 }
 
 export default App;
