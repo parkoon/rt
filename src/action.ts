8691e1a8c32ee4f85ad86c80842397cace8fcfc0
jayden <jayden@kcd.co.kr>
2021-07-16 03:08:30 +0900

    todo to issue

---
+++ src/action.ts
@@ -0,0 +1,17 @@
+/**
+ * TODO action test one title
+ * action test two description
+ */
+const actionTestOne = () => {};
+
+// TODO. action test two title
+// action test two description
+// action test more
+const actionTestTwo = () => {};
+
+// TODO action test three title
+// foo
+// bar
+// hello
+// world
+const actionTestThree = () => {};
