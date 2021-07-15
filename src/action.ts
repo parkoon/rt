ae0a45fbb90bcd92cf3e90b647c5b9e97cc9b602
jayden <jayden@kcd.co.kr>
2021-07-16 03:11:46 +0900

    remove todo

--- src/action.ts
+++ src/action.ts
@@ -1,17 +1,5 @@
-/**
- * TODO action test one title
- * action test two description
- */
 const actionTestOne = () => {};
 
-// TODO. action test two title
-// action test two description
-// action test more
 const actionTestTwo = () => {};
 
-// TODO action test three title
-// foo
-// bar
-// hello
-// world
 const actionTestThree = () => {};
